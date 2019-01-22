53f580d36dd73149247bee03552e0e5707923d62
Kartk Vashistha <kartikv.official@gmail.com>
2019-01-22 05:08:29 +0000

    Task 1 and 2 complete

--- Lab01Basics/Lab01Code.py
+++ Lab01Basics/Lab01Code.py
@@ -32,10 +32,14 @@ from petlib.cipher import Cipher
 
 def encrypt_message(K, message):
     """ Encrypt a message under a key K """
-
-    plaintext = message.encode("utf8")
-    
     ## YOUR CODE HERE
+    plaintext = message.encode("utf8")
+          
+    aes = Cipher("aes-128-gcm") # Initialize AES-GCM with 128 bit keys
+    iv  = urandom(16)
+    # Encryption using AES-GCM returns a ciphertext and a tag
+    ciphertext, tag = aes.quick_gcm_enc(K, iv, plaintext)
+    
 
     return (iv, ciphertext, tag)
 
@@ -45,7 +49,9 @@ def decrypt_message(K, iv, ciphertext, tag):
         In case the decryption fails, throw an exception.
     """
     ## YOUR CODE HERE
-
+    aes = Cipher("aes-128-gcm") # Initialize AES-GCM with 128 bit keys
+    plain = aes.quick_gcm_dec(K, iv, ciphertext, tag)
+    
     return plain.encode("utf8")
 
 #####################################################
